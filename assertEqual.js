8405c8179d307fc1b370e5d5934a3910facef684
Sahil Athia <sahilathia918@gmail.com>
2020-10-26 19:16:47 +0000

    creating a function to mimic console.assert

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertation Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
   } else {
-    console.log("❌❌❌ Assertation Failed: " + actual + " !== " + expected);
+    console.log(`❌❌❌ Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Bootcamp", "Bootcamp");
