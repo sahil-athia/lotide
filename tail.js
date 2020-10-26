088d18fe3323fc541d70fcf0ee437bc4faf4cc0c
Sahil Athia <sahilathia918@gmail.com>
2020-10-26 20:25:45 +0000

    assertation on tails

---
+++ tail.js
@@ -0,0 +1,24 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let tempArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i !== 0) {
+      tempArray.push(array[i]);
+    }
+  }
+  return tempArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
