81689e06f511d9e73b0a23a000da0c744c9a5dca
Sahil Athia <sahilathia918@gmail.com>
2020-10-27 23:20:40 +0000

    remove unwanted elements from an array

--- without.js
+++ without.js
@@ -1,6 +1,35 @@
+const eqArrays = function(array1, array2) {
+  let arrayMatch = false;
+  if (array1.length === array2.length) {
+    if (array1.length === 0 && array2.length === 0) {
+      arrayMatch = true;
+      return arrayMatch;
+    } 
+    for (let i = 0; i < array1.length; i++) {
+    //iterate through array
+      if (array1[i] === array2[i]) {
+        //strict equals operator
+        arrayMatch = true;
+      } else {
+        arrayMatch = false;
+        return arrayMatch;
+        //return false at first mismatch
+      }
+    }
+  } else {
+    return arrayMatch;
+    //return false if array lengths are different
+  }
+  return arrayMatch;
+};
+
 const assertArraysEqual = function(array1, array2) {
   let arrayMatch = `❌❌❌ Assertation Failed: ${array1} !== ${array2}`;
   if (array1.length === array2.length) {
+    if (array1.length === 0 && array2.length === 0) {
+      arrayMatch = `✅✅✅ Assertation Passed: ${array1} === ${array2}`;
+      return console.log(arrayMatch);
+    }
     for (let i = 0; i < array1.length; i++) {
     //iterate through array
       if (array1[i] === array2[i]) {
