37a648ad0a6a4e7815af449f7f46e6ad5aca802c
Sahil Athia <sahilathia918@gmail.com>
2020-10-28 17:38:10 +0000

    return object of letters in a string

--- countLetters.js
+++ countLetters.js
@@ -8,17 +8,24 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(letters) {
   let lettersObj = {};
+  //  create an object to store the letters that we find
 
   for (const letter of letters) {
+    // loop through each of the letters in out string
     if (letter !== " ") {
+    // ignore any blank spaces
       if (lettersObj[letter]) {
         lettersObj[letter] += 1;
+        // if the letter exists in our object, add a number
       } else {
         lettersObj[letter] = 1;
+        // if the letter does not exit in object, add it 
       }
     }
   }
   return lettersObj;
+  // return our object
 };
 
-console.log(countLetters("hello   hello"));
+// TEST : 
+// console.log(countLetters("hello"));
