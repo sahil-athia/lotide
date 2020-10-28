ff244f6b0e5a753a3008e4beffe996c3ffcc897d
Sahil Athia <sahilathia918@gmail.com>
2020-10-28 23:01:38 +0000

    middle function for arrays

--- middle.js
+++ middle.js
@@ -53,9 +53,12 @@ const assertArraysEqual = function(array1, array2) {
 const middle = function(array) {
   let middleArray = [];
   let middleIndex = Math.floor(array.length / 2)
+  // store the middle index in a variable
   if (array.length > 2) {
+    // arrays of less than 3 dont have a middle
     if (array.length % 2 === 0) {
       middleArray.push(array[middleIndex - 1]);
+      // the even middle index is the second of the 2 middles
       middleArray.push(array[middleIndex]);
     } else {
       middleArray.push(array[middleIndex]);
