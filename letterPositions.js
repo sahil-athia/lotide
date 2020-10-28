440c8b6164a1cd99dffa34ee5b5fbd772ae71e05
Sahil Athia <sahilathia918@gmail.com>
2020-10-28 18:12:32 +0000

    addding the index of reccuring letters into an object

--- letterPositions.js
+++ letterPositions.js
@@ -50,13 +50,17 @@ const assertArraysEqual = function(array1, array2) {
 
 const letterPositions = function(sentance) {
   const results = {};
+  // object to store the indexes
 
   for (let i = 0; i < sentance.length; i++) {
     if (sentance[i] !== " ") {
+      // ignore spaces
       if (results[sentance[i]]){
         results[sentance[i]].push(i)
+        //push the index of letters that are already in the object
       } else {
         results[sentance[i]] = []
+        //create an array for letters not in the object
         results[sentance[i]].push(i)
       }
     }
