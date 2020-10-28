8c4ba0cefcbc93ef170031e958cbab1f9d1cd118
Sahil Athia <sahilathia918@gmail.com>
2020-10-28 21:42:46 +0000

    function to log assertation of 2 objects conclusion directly

---
+++ assertObjectsEqual.js
@@ -0,0 +1,35 @@
+const eqObjects = function(object1, object2) {
+  let final = false;
+  // create a variable to store and change the outcome
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    // if the number of keys are the same, proceed, else return false
+    for (let key of Object.keys(object1)) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        final = eqArrays(object1[key], object2[key]);
+        //check to see if the key-values are arrays and pass to eqArrays
+      } else if (object1[key] === object2[key]) {
+        final = true;
+        // if the key-values are not arrays but still the same, pass true to the outcome
+      } else {
+        return false;
+        // if the above conditions do not pass, return false
+      }
+    }
+    return final;
+    // return the outcome at the end of the conditionals
+  } else {
+    return false;
+  }
+};
+
+const assertFunctionsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
+  let outcome = eqObjects(actual, expected)
+  if (outcome === true) {
+    console.log(`✅✅✅ Assertation Passed: ${inspect(actual)} === ${inspect(expected)}`)
+  } else {
+    console.log(`❌❌❌ Assertation Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+}
+
+//assertFunctionsEqual({1: "a", 2: "b"}, {1: "a", 2: "b"})
