49242083762f7eae2fc820100b3e070f3dc0224e
Sahil Athia <sahilathia918@gmail.com>
2020-11-02 14:20:43 +0000

    refactored code

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let tempArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -16,8 +8,4 @@ const tail = function(array) {
   return tempArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
