112569c9e0c522803594262c5e8a23c98ff01f65
Sahil Athia <sahilathia918@gmail.com>
2020-11-02 17:37:25 +0000

    refactored the code

--- middle.js
+++ middle.js
@@ -1,37 +1,3 @@
-const eqArrays = function(array1, array2) {
-  let arrayMatch = false;
-  if (array1.length === array2.length) {
-    if (array1.length === 0 && array2.length === 0) {
-      arrayMatch = true;
-      return arrayMatch;
-    } 
-    for (let i = 0; i < array1.length; i++) {
-    //iterate through array
-      if (array1[i] === array2[i]) {
-        //strict equals operator
-        arrayMatch = true;
-      } else {
-        arrayMatch = false;
-        return arrayMatch;
-        //return false at first mismatch
-      }
-    }
-  } else {
-    return arrayMatch;
-    //return false if array lengths are different
-  }
-  return arrayMatch;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let outcome = eqArrays(actual, expected);
-  if (outcome === true) {
-    console.log(`✅✅✅ Assertation Passed: ${(actual)} is equal to ${(expected)}`);
-  } else {
-    console.log(`❌❌❌ Assertation Failed: ${(actual)} is not equal ${(expected)}`);
-  }
-};
-
 const middle = function(array) {
   let middleArray = [];
   let middleIndex = Math.floor(array.length / 2)
@@ -51,12 +17,5 @@ const middle = function(array) {
   return middleArray;
 };
 
+module.exports = middle;
 
-// TESTS
-// console.log(middle([1]))
-// console.log(middle([1, 2]))
-// console.log(middle([1, 2, 3]))
-// console.log(middle([1, 2, 3, 4, 5]))
-// console.log(middle([1, 2, 3, 4]))
-// console.log(middle([1, 2, 3, 4, 5, 6,]))
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
