652c752fdff85b204c7d1f75418356386378ae4b
Sahil Athia <sahilathia918@gmail.com>
2020-11-02 14:12:51 +0000

    modularized code

--- head.js
+++ head.js
@@ -1,17 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const head = function(array) {
   return array[0];
 };
 
-/* TESTS
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([]));
-*/
+module.exports = head;
+
+
